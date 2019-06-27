c1880152272864f3a3a2d490fb30df94e5565035
Christian Lanning <christian.lanning@navy.mil>
2019-06-27 09:24:44 -0400

    added textencrypter project

---
+++ TextEncrypter/TextEncrypter/Cipher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEncrypter
+{
+    class Cipher
+    {
+        private string text;
+        public Cipher(string t) => text = t;
+        public void Encrypt()
+        {
+
+        }
+        public void Decrypt()
+        {
+
+        }
+
+    }
+}
---
+++ TextEncrypter/TextEncrypter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEncrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Cipher c;
+            Console.WriteLine("Lanning Cipher");
+            while (true)
+            {
+                Console.Write("Encrypt or Decrypt(e/d)?: ");
+                if (Console.Read() == 'e')
+                {
+                    Console.Write("Enter text to Encrypt: ");
+                    text = Console.ReadLine();
+                    c = new Cipher(text);
+                    c.Encrypt();
+                }
+                else if (Console.Read() == 'd')
+                {
+                    Console.Write("Enter text to Decrypt: ");
+                    text = Console.ReadLine();
+                    c = new Cipher(text);
+                    c.Decrypt();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+    }
+}
