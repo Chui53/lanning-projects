793d0f73693881745e54371ef7cbb431e9e0970b
Christian Lanning <christian.lanning@navy.mil>
2019-06-27 15:28:01 -0400

    updated tictactoe project

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        private int[,] boardArray = new int[3, 3];
+        public Board() { }
+        public void Clear()
+        {
+            Array.Clear(boardArray, 0, boardArray.Length);
+        }
+        public void Play(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    if(boardArray[0,0] == 0)
+                    {
+                        boardArray[0, 0] = 2;
+                    } else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 2:
+                    if (boardArray[0, 1] == 0)
+                    {
+                        boardArray[0, 1] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 3:
+                    if (boardArray[0, 2] == 0)
+                    {
+                        boardArray[0, 2] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 4:
+                    if (boardArray[1, 0] == 0)
+                    {
+                        boardArray[1, 0] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 5:
+                    if (boardArray[1, 1] == 0)
+                    {
+                        boardArray[1, 1] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 6:
+                    if (boardArray[1, 2] == 0)
+                    {
+                        boardArray[1, 2] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 7:
+                    if (boardArray[2, 0] == 0)
+                    {
+                        boardArray[2, 0] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 8:
+                    if (boardArray[2, 1] == 0)
+                    {
+                        boardArray[2, 1] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                case 9:
+                    if (boardArray[2, 2] == 0)
+                    {
+                        boardArray[2, 2] = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal Play");
+                    }
+                    break;
+                default:
+
+                    break;
+            }
+        }
+        private void Check()
+        {
+
+        }
+        private void AI()
+        {
+
+        }
+
+    }
+}
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -10,7 +10,26 @@ namespace TicTacToe
     {
         static void Main()
         {
-
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+            Console.WriteLine("You will be facing against an AI opponent who will be playing X.");
+            Console.WriteLine("The AI will always go first.");
+            Console.WriteLine("The board looks like this:");
+            Console.WriteLine(
+                "   |   |   \n" +
+                " 1 | 2 | 3 \n" +
+                "   |   |   \n" +
+                "---+---+---\n" +
+                "   |   |   \n" +
+                " 4 | 5 | 6 \n" +
+                "   |   |   \n" +
+                "---+---+---\n" +
+                "   |   |   \n" +
+                " 7 | 8 | 9 \n" +
+                "   |   |   ");
+            Console.WriteLine("Each square is represented by a number.");
+            Console.WriteLine("This is how you will input your moves.");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
