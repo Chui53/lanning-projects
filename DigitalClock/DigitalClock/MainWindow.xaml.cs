76817b9a67c58f367ce1b336636f7c41c2120875
Christian Lanning <christian.lanning@navy.mil>
2019-07-01 13:51:12 -0400

    updated digitalclock project

--- DigitalClock/DigitalClock/MainWindow.xaml.cs
+++ DigitalClock/DigitalClock/MainWindow.xaml.cs
@@ -22,37 +22,87 @@ namespace DigitalClock
     public partial class MainWindow : Window
     {
         DispatcherTimer Timer = new DispatcherTimer();
+        DateTime currentTime;
+        TimeZoneInfo timeInfo;
+        bool east;
+        bool west;
+        bool cent;
+        bool fore;
         public MainWindow()
         {
             InitializeComponent();
-            Timer.Interval = new TimeSpan(0, 0, 1);
-            Timer.Tick += new EventHandler(timer_Tick);
-            Timer.Start();           
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");        
+            east = true;
+            Timer.Interval = TimeSpan.FromMilliseconds(1);
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Start();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            Time.Text = DateTime.Now.ToString("HH:mm:ss");
+            currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeInfo);
+            Time.Text = currentTime.ToString("HH:mm:ss");
+            if (east)
+            {
+                USEastern.IsEnabled = false;
+                USWestern.IsEnabled = true;
+                USCentral.IsEnabled = true;
+                Foreign.IsEnabled = true;
+            } else if (west)
+            {
+                USEastern.IsEnabled = true;
+                USWestern.IsEnabled = false;
+                USCentral.IsEnabled = true;
+                Foreign.IsEnabled = true;
+            } else if (cent)
+            {
+                USEastern.IsEnabled = true;
+                USWestern.IsEnabled = true;
+                USCentral.IsEnabled = false;
+                Foreign.IsEnabled = true;
+            } else if (fore)
+            {
+                USEastern.IsEnabled = true;
+                USWestern.IsEnabled = true;
+                USCentral.IsEnabled = true;
+                Foreign.IsEnabled = false;
+            }
         }
 
         private void Eastern_Click(object sender, RoutedEventArgs e)
         {
-
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
+            east = true;
+            cent = false;
+            west = false;
+            fore = false;
         }
 
         private void Central_Click(object sender, RoutedEventArgs e)
         {
-
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            cent = true;
+            east = false;
+            west = false;
+            fore = false;
         }
 
         private void Western_Click(object sender, RoutedEventArgs e)
         {
-
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            west = true;
+            east = false;
+            cent = false;
+            fore = false;
         }
 
         private void Foreign_Click(object sender, RoutedEventArgs e)
         {
-
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            fore = true;
+            east = false;
+            west = false;
+            cent = false;
         }
     }
 }
