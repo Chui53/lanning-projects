12a636fc7ce8297a03589fcf1afcb541c00f6182
Christian Lanning <christian.lanning@navy.mil>
2019-07-09 10:03:05 -0400

    added extensionsorter project

--- DigitalClock/DigitalClock/MainWindow.xaml.cs
+++ DigitalClock/DigitalClock/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace DigitalClock
@@ -24,15 +13,16 @@ namespace DigitalClock
         DispatcherTimer Timer = new DispatcherTimer();
         DateTime currentTime;
         TimeZoneInfo timeInfo;
-        bool east;
-        bool west;
-        bool cent;
-        bool fore;
+        ReadOnlyCollection<TimeZoneInfo> timezones;
         public MainWindow()
         {
             InitializeComponent();
-            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");        
-            east = true;
+            timezones = TimeZoneInfo.GetSystemTimeZones();
+            foreach (TimeZoneInfo tz in timezones)
+            {
+                TimeBox.Items.Add(tz.Id);
+            }
+            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
             Timer.Interval = TimeSpan.FromMilliseconds(1);
             Timer.Tick += new EventHandler(Timer_Tick);
             Timer.Start();
@@ -40,69 +30,13 @@ namespace DigitalClock
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            if(TimeBox.SelectedValue != null)
+            {
+                timeInfo = TimeZoneInfo.FindSystemTimeZoneById(TimeBox.SelectedValue.ToString());
+            }
             currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeInfo);
             Time.Text = currentTime.ToString("HH:mm:ss");
-            if (east)
-            {
-                USEastern.IsEnabled = false;
-                USWestern.IsEnabled = true;
-                USCentral.IsEnabled = true;
-                Foreign.IsEnabled = true;
-            } else if (west)
-            {
-                USEastern.IsEnabled = true;
-                USWestern.IsEnabled = false;
-                USCentral.IsEnabled = true;
-                Foreign.IsEnabled = true;
-            } else if (cent)
-            {
-                USEastern.IsEnabled = true;
-                USWestern.IsEnabled = true;
-                USCentral.IsEnabled = false;
-                Foreign.IsEnabled = true;
-            } else if (fore)
-            {
-                USEastern.IsEnabled = true;
-                USWestern.IsEnabled = true;
-                USCentral.IsEnabled = true;
-                Foreign.IsEnabled = false;
-            }
-        }
-
-        private void Eastern_Click(object sender, RoutedEventArgs e)
-        {
-            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
-            east = true;
-            cent = false;
-            west = false;
-            fore = false;
-        }
-
-        private void Central_Click(object sender, RoutedEventArgs e)
-        {
-            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
-            cent = true;
-            east = false;
-            west = false;
-            fore = false;
-        }
-
-        private void Western_Click(object sender, RoutedEventArgs e)
-        {
-            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
-            west = true;
-            east = false;
-            cent = false;
-            fore = false;
-        }
-
-        private void Foreign_Click(object sender, RoutedEventArgs e)
-        {
-            timeInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
-            fore = true;
-            east = false;
-            west = false;
-            cent = false;
+            
         }
     }
 }
---
+++ ExtensionSorter/ExtensionSorter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ExtensionSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            List<string> fileNames = new List<string>();
+            List<string> fileTypes = new List<string>();
+            // Adding paths and extensions to their respective lists.
+            foreach(string file in Directory.GetFiles(path))
+            {
+                if (Path.GetExtension(file) != ".exe")
+                {
+                    fileNames.Add(file);
+                }
+                if (!fileTypes.Contains(Path.GetExtension(file)) && Path.GetExtension(file) != ".exe")
+                {
+                    fileTypes.Add(Path.GetExtension(file));
+                }
+            }
+            // Create new folders for the different extensions.
+            foreach(string type in fileTypes)
+            {
+                Directory.CreateDirectory(type);
+            }
+            // Moves files into their respective folders based on extension.
+            foreach(string file in fileNames)
+            {
+                string destPath = Directory.GetCurrentDirectory();
+                destPath += ("\\" + Path.GetExtension(file) + "\\" + Path.GetFileName(file));
+                File.Move(file, destPath);
+            }
+        }
+    }
+}
